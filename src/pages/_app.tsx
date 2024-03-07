545bc7e17b90fca7012a659f3e003aab02ad14d2
Gilang Ramadhan <gilangramadhanii191@gmail.com>
2024-03-07 09:50:07 +0700

    feat(app): add theme provider and base class

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,35 @@
 import type { AppProps } from 'next/app';
+import { Inter, Poppins } from 'next/font/google';
+
+import { ThemeProvider } from '@/providers/ThemeProvider';
+
+import { cn } from '@/libs/utils';
 
 import '@/styles/globals.css';
 
+const poppins = Poppins({
+  variable: '--font-poppins',
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+      <main
+        className={cn(
+          'min-h-screen bg-background antialiased font-sans',
+          inter.variable,
+          poppins.variable
+        )}
+      >
+        <Component {...pageProps} />
+      </main>
+    </ThemeProvider>
+  );
 }
